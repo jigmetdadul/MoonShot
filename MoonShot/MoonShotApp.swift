ceba32734b663a413012f1381dec38347d596c50
Jigmet Dadul <85403349+jigmetdadul@users.noreply.github.com>
2024-02-08 17:30:54 +0530

    Initial Commit

---
+++ MoonShot/MoonShotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonShotApp.swift
+//  MoonShot
+//
+//  Created by Jigmet stanzin Dadul on 08/02/24.
+//
+
+import SwiftUI
+
+@main
+struct MoonShotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
