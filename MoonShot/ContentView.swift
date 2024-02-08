00daac1a619670de9644e25d8ee2a75e108fe071
Jigmet Dadul <85403349+jigmetdadul@users.noreply.github.com>
2024-02-08 18:06:40 +0530

    adding scrollView

--- MoonShot/ContentView.swift
+++ MoonShot/ContentView.swift
@@ -7,15 +7,26 @@
 
 import SwiftUI
 
+struct CustomTextView: View {
+    var text: String
+    var body: some View {
+        Text(text)
+    }
+    init(text: String) {
+        print("Custom View creating \(text)")
+        self.text = text
+    }
+}
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ScrollView{
+            LazyVStack(spacing: 10){
+                ForEach(0..<50){ ele in
+                    CustomTextView(text: "My Text \(ele)")
+                }
+            }
+            .frame(maxWidth: .infinity)
         }
-        .padding()
     }
 }
 
